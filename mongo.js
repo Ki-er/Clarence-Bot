92c9acc3b6ad6b44beb5161ffe6e07b391ad6603
kieran <Kieranrobson1999@gmail.com>
2022-02-28 11:09:27 +0000

    removed brackets around mongoose connection

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { mongooseConnectionString } = process.env.DJS_MONGO
+const mongooseConnectionString = process.env.DJS_MONGO
 
 module.exports = async () => {
   await mongoose.connect(mongooseConnectionString, {
