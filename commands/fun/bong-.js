81b093930cc5bbe6024904a3298d12f5d1d4712f
kieranrobson <kieranrobson1999@gmail.com>
2022-05-01 12:38:13 +0100

    Added bong command

---
+++ commands/fun/bong-.js
@@ -0,0 +1,28 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+const discord = require("discord.js");
+
+
+module.exports = {
+    name: "bong",
+    aliases: [''],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+
+        const Responses = [
+            "<@365297607276363776>", 
+            "<@544191902946164756>",
+            ];
+
+            let atta = new discord.MessageAttachment("https://c.tenor.com/9kwNAT9bTkEAAAAC/bong-smoke.gif");
+            message.channel.send({content: `Time to hit up: ${Responses[Math.floor(Math.random() * Responses.length)]}`, files: [atta]})
+            
+        }
+        
+    }
+
+
