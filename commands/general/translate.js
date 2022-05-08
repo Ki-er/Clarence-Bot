065b00ff10f8e09298e87ea664dd54d5515b01a7
kieranrobson <kieranrobson1999@gmail.com>
2022-05-09 00:21:01 +0100

    Added message.author,tag to tr embed

--- commands/general/translate.js
+++ commands/general/translate.js
@@ -26,7 +26,7 @@ module.exports = {
         .addField(`**Translated Text:**`, `${res.text.charAt(0).toUpperCase()}${res.text.slice(1)}`)
         .addField(`**Originial Text:**`, `${txt.charAt(0).toUpperCase()}${txt.slice(1)}`)
         .addField(`**Language:**`, `${language.charAt(0).toUpperCase()}${language.slice(1)}`)
-        .setFooter(`ID: ${message.author.id}`)
+        .setFooter(`Called By: ${message.author.tag}`)
         .setTimestamp()
 
         message.channel.send({ embeds: [embed] });
