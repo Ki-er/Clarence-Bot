13ca843a193e44a9b4b04c68e0a436505cf93ba8
kieranrobson <kieranrobson1999@gmail.com>
2022-05-10 00:32:15 +0100

    Added buy command

---
+++ commands/siege/buy.js
@@ -0,0 +1,21 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'buy',
+    aliases: [''],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+
+        const embed = new MessageEmbed()
+        .setColor('ORANGE')
+        .setFooter(`Called By: ${message.author.tag}`)
+        .setTimestamp()
+        .setTitle("Purchase")
+        .addField(`While Kieran and others are not affiliated with CD Keys, we have had good expiriences with CD keys, buy siege here: `, `https://www.cdkeys.com/tom-clancy-s-rainbow-six-siege-pc-cd-key-uplay`)
+        message.channel.send({ embeds: [embed] })
+    }
+}
