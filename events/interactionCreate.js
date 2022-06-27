b9ebc3228c66a16305a0e579e926590474b88c8c
KieranRobson <kieranrobson1999@gmail.com>
2022-06-27 15:40:29 +0100

    RM defer reply

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,8 +5,6 @@ const { channels } = require("../index");
 client.on("interactionCreate", async (interaction) => {
     // Slash Command Handling
     if (interaction.isCommand()) {
-        await interaction.deferReply({ ephemeral: false }).catch(() => {});
-
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
             return interaction.followUp({ content: "An error has occured " });
