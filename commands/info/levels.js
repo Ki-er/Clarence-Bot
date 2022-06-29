4955c2511e81dc3e5b21d6d64be283087afa0fab
KieranRobson <kieranrobson1999@gmail.com>
2022-06-29 15:34:01 +0100

    Test comit

--- commands/info/levels.js
+++ commands/info/levels.js
@@ -13,16 +13,13 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message, args) => {
-        const leaderboard = await profile.find({guildId: message.guildId}).sort({level : -1}).limit(10);;
+        const leaderboard = await profile.find({guildId: message.guildId, userId: message.author.id});
             const embed = new MessageEmbed()
                 .setColor('ORANGE')
                 .setFooter(`Called By: ${message.author.tag}`)
                 .setTimestamp()
+                .setTitle(``)
 
-                for(let i = 0; i < leaderboard.length; ++i) {
-                    embed.addField(`${i+1}`, `<@${leaderboard[i].userId}> - Level: ${leaderboard[i].level}`)
-                }
-                embed.setTitle(`The Top ${leaderboard.length} levels`)
 
 
             message.channel.send({ embeds: [embed] });
