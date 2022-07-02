5cd1c4b6ba39322cb4e654f0bf5f02eca6e4b6ff
Kieran <kieranrobson1999@gmail.com>
2022-07-02 12:32:40 +0100

    Fixed kick and ban commands

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,52 +1,24 @@
-const { Discord, Client, Message, MessageEmbed, Permissions } = require('discord.js');
+const { Message, Client, Permissions } = require("discord.js");
 
 module.exports = {
-    name: 'ban',
+    name: "ban",
     aliases: [''],
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
      */
-    run: async(client, message, args) => {
+    run: async (client, message, args) => {
 
-      try {
-      
-        const member = message.mentions.members.first();
-        const permission = message.member.permissions.has(Discord.Permissions.FLAGS.BAN_MEMBERS)
-      
-        if (!permission)
-          return message.reply({ 
-              contents: "You don't have permission to use this command"
-          });
-      
-        if (!args[0]) return message.reply({ content: `Please specify a user to ban` });
-      
-        if (!member) return message.reply({ content: `💤 | Cannot find that user...` } );
-      
-        if (member.id === message.author.id)
-          return message.reply({ content: `You cannot ban yourself!` });
-      
-        if (message.member.roles.highest.position < member.roles.highest.position)
-          return message.reply({
-            content: `You cannot ban user who have higher role than you...`
-          });
-      
-        if (!member.bannable) return message.reply({ content: `I cannot ban that member`});
-      
-        return (
-          (await member.ban()) +
-          message
-            .reply({
-              content: `User ${member} has been banned`
-            })
-            .then((msg) => {
-              setTimeout(() => msg.delete(), 5000);
-            })
-        );
-          } catch(err) {
-            message.reply({ content: `This command is currently being worked on.` })
-          }
 
-    }
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if(!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS, Permissions.FLAGS.ADMINISTRATOR)) return message.reply("You don't have permission to use that command.")
+    if (!args[0]) return message.reply({ content: `Please specify a user to ban` });
+    if(member.id == message.author.id) return message.reply("You cant ban yourself!")
+    
+    return ((await member.ban()) + message.reply({content: `User ${member} has been banned`}))
+
+  }
 }
+
--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -11,45 +11,13 @@ module.exports = {
      */
     run: async (client, message, args) => {
 
-    // the perm. that the member need it to ban someone
-    if(!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS, Permissions.FLAGS.ADMINISTRATOR))
-    // if someone dont hv perm it will send this message
-    message.channel.send("You don't have permission to use that command.");
 
-    else {
-      if (!message.guild) return;
-  
-      const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-  
-      if (user) {
-  
-        const member =message.guild.members.cache.get(user.id);
-  
-        if (member) {
-  
-          member
-          // kick code 
-            .kick('they were bad!')
-            .then(() => {
-            // it will send this message once the person is kicked
-              message.reply(`Successfully kicked`);
-            })
-            // log err in the console
-            .catch(err => {
-              // if the bot wasnt able to kick the member bcz he hv a higher role it will not kick him and if the bot dont hv to perm it will not kick him and send this messge
-              message.reply('I was unable to kick the member');
-  
-              console.error(err);
-            });
-        } else {
-          // if the member isnt in the server and u typed e.g. =kick @karimx it will send this message
-          message.reply("That user isn't in this guild!");
-        }
-      } else {
-       // if u typed =kick without mentioning some1 it will send this message
-        message.reply("You didn't mention the user to kick!");
-      }
-  };
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if(!message.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) return message.reply("You don't have permission to use that command.")
+    if (!args[0]) return message.reply({ content: `Please specify a user to kick` });
+    if(member.id == message.author.id) return message.reply("You cant kick yourself!")
+    
+    return ((await member.kick()) + message.reply({content: `User ${member} has been kicked`}))
 
-    },
-};
+  }
+}
