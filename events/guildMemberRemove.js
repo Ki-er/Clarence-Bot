506b9fdf52f03828d835ce4fa3632435b5fb72d6
Kieran <kieranrobson1999@gmail.com>
2022-07-02 16:58:24 +0100

    Added protection to guildmemberremove

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,9 @@
 const client = require("../index");
 const discord = require('discord.js');
 client.on('guildMemberRemove', guildmember => {
-    guildmember.guild.channels.cache.get('744594748815310930').send(`<:bye:875359507503546438> Goodbye **${guildmember.user.tag}** has just left server!`)
-    
+    if(guildmember.guild.id == "744586833135927366")
+    {
+        guildmember.guild.channels.cache.get('744594748815310930').send(`<:bye:875359507503546438> Goodbye **${guildmember.user.tag}** has just left server!`)
+
+    }
 });
