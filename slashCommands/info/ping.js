db785c260f4084c623099620fcf6072eee480b76
Kieran <kieranrobson1999@gmail.com>
2022-07-24 19:51:11 +0100

    Fixed ping command

--- slashCommands/info/ping.js
+++ slashCommands/info/ping.js
@@ -1,8 +1,8 @@
-const { Client, CommandInteraction } = require("discord.js");
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "ping",
-    description: "returns websocket ping",
+    description: "returns ping to Clarence",
     type: 'CHAT_INPUT',
     /**
      *
@@ -11,6 +11,18 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, interaction, args) => {
-        interaction.followUp({ content: `${client.ws.ping}ms!` });
+        let days = Math.floor(client.uptime / 86400000);
+        let hours = Math.floor(client.uptime / 3600000) % 24;
+        let minutes = Math.floor(client.uptime / 60000) % 60;
+        let seconds = Math.floor(client.uptime / 1000) % 60;
+
+        const embed = new MessageEmbed()
+        .setColor('ORANGE')
+        .setFooter({ text: `Called By: ${interaction.user.tag}`})
+        .setTimestamp()
+        .setTitle("Pong!")
+        .setDescription(`${client.ws.ping} ping to host`)
+        .addField('Uptime', ` ${days}days ${hours}hrs ${minutes}min ${seconds}sec`, true)
+        interaction.reply({ embeds: [embed]});
     },
 };
