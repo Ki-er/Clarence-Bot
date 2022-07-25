2374272421d7272712894ea1c7f1c39432825fb6
Kieran <kieranrobson1999@gmail.com>
2022-07-25 19:36:54 +0100

    edit slash commands

---
+++ slashCommands/fun/blamescan.js
@@ -0,0 +1,32 @@
+const { Message, MessageActionRow, MessageButton  } = require("discord.js");
+const client = require("../../index");
+const blames = require('../../schemas/blamestitch-schema');
+
+module.exports = {
+    name: "blamescan",
+    description: "Scan blames",
+    type: 'CHAT_INPUT',
+    /**
+     *
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        const blame = await blames.find({});
+        console.log(blame)
+        interaction.reply(`Stitch has been blamed ${19 + blame.length} times.\r\nThere were 19 blames before the database.\r\nGetting all database blames, this may take a while.`)
+
+
+        for(let i = 0; i < blame.length; ++i) {
+            const embed = new MessageEmbed()
+                .setColor('ORANGE')
+                .setFooter({ text: `Called By: ${message.author.tag}`})                   
+                .setTimestamp()
+                .addField('Reason:', `${blame[i].reason}`)
+                .addField('Date', `${blame[i].date.toLocaleDateString("en-UK")}`)
+                .addField('Added by', `<@${blame[i].userId}>`);
+            interaction.channel.send({ embeds: [embed] });
+        }
+    }
+};
--- slashCommands/info/leaderboard.js
+++ slashCommands/info/leaderboard.js
@@ -28,4 +28,4 @@ module.exports = {
         interaction.reply({ embeds: [embed] })
 
     },
-};
+}; 
