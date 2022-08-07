73e27f6ad8b3b903ae909e4dcb05dfc16763d587
Kieran <kieranrobson1999@gmail.com>
2022-08-07 10:20:01 -0700

    Clean up handler

--- handler/index.js
+++ handler/index.js
@@ -11,36 +11,41 @@ const globPromise = promisify(glob);
 module.exports = async (client) => {
     // Commands
     const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
-    commandFiles.map((value) => {
-        const file = require(value);
-        const splitted = value.split("/");
-        const directory = splitted[splitted.length - 2];
-
-        if (file.name) {
-            const properties = { directory, ...file };
-            client.commands.set(file.name, properties);
-        }
-    });
+        commandFiles.map((value) => {
+            console.log(`Load Command: ${value}`)
+            const file = require(value);
+            const splitted = value.split("/");
+            const directory = splitted[splitted.length - 2];
+    
+            if (file.name) {
+                const properties = { directory, ...file };
+                client.commands.set(file.name, properties);
+            }
+        });
 
     // Events
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
-    eventFiles.map((value) => require(value));
+    for(const filepath of eventFiles) {
+        eventFiles.map((value) => require(value));
+        console.log(`Load Event: ${filepath}`)
+    }
 
     // Slash Commands
     const slashCommands = await globPromise(`${process.cwd()}/SlashCommands/*/*.js`);
-
     const arrayOfSlashCommands = [];
-    slashCommands.map((value) => {
-        const file = require(value);
-        if (!file?.name) return;
-        client.slashCommands.set(file.name, file);
+        slashCommands.map((value) => {
+            console.log(`Load Slash Command: ${value}`)
+            const file = require(value);
+            if (!file?.name) return;
+            client.slashCommands.set(file.name, file);
+        
+            if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+            arrayOfSlashCommands.push(file);
+        })
+    
 
-        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
-        arrayOfSlashCommands.push(file);
-    });
     client.on("ready", async () => {
-        console.log("Slash commands registered!")
-        // Register for all the guilds the bot is in
         await client.application.commands.set(arrayOfSlashCommands);
+        console.log("Slash commands registered!")
     });
 };
