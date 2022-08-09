7523b6adb5b9ed58090e1e5c0021d4e987e78269
KieranRobson <kieranrobson1999@gmail.com>
2022-08-09 11:40:58 +0100

    RM XP levelling

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,9 @@
 const client = require("../index");
-const levels = require('../events/levels')
-
-
 
 client.on('ready', () => {
     console.log('Clarence is online')
     client.user.setActivity(`-help`, {type:"WATCHING"})
-    levels(client)
-
-
+    
     const guild = client.guilds.cache.get('744586833135927366');
     setInterval(() =>{
         const memberCount = guild.members.cache.filter(m => !m.user.bot).size;
