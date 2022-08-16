63c571f2e2d938a5ddd92e712a777af0ec7c9d40
Kieran <kieranrobson1999@gmail.com>
2022-08-16 02:52:54 -0700

    Fix clarence not connecting to db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Collection } = require("discord.js");
+const mongoose = require('mongoose')
 require("dotenv").config();
 
 const client = new Client({
@@ -12,5 +13,15 @@ client.slashCommands = new Collection();
 
 // Initializing the project
 require("./handler")(client);
+
+const mongooseConnectionString = process.env.MONGOOSE
+
+mongoose.connect(mongooseConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log("Connected to db")
+
+
 
 client.login(process.env.DISCORD_TOKEN);
