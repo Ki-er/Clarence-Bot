1d657fef006c8a5bb5c782f5903a9e642465961c
Kieran <kieranrobson1999@gmail.com>
2022-08-16 16:42:35 +0100

    Test hullcss handler

--- handler/index.js
+++ handler/index.js
@@ -1,9 +1,9 @@
+
 const { glob } = require("glob");
 const { promisify } = require("util");
 const { Client } = require("discord.js");
 
 const globPromise = promisify(glob);
-
 
 /**
  * @param {Client} client
@@ -11,41 +11,43 @@ const globPromise = promisify(glob);
 module.exports = async (client) => {
     // Commands
     const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
-        commandFiles.map((value) => {
-            console.log(`Load Command: ${value}`)
-            const file = require(value);
-            const splitted = value.split("/");
-            const directory = splitted[splitted.length - 2];
-    
-            if (file.name) {
-                const properties = { directory, ...file };
-                client.commands.set(file.name, properties);
-            }
-        });
+    commandFiles.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+
+        if (file.name) {
+            const properties = { directory, ...file };
+            client.commands.set(file.name, properties);
+        }
+    });
 
     // Events
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
-    for(const filepath of eventFiles) {
-        eventFiles.map((value) => require(value));
-        console.log(`Load Event: ${filepath}`)
-    }
+    eventFiles.map((value) => require(value));
 
     // Slash Commands
-    const slashCommands = await globPromise(`${process.cwd()}/SlashCommands/*/*.js`);
-    const arrayOfSlashCommands = [];
-        slashCommands.map((value) => {
-            console.log(`Load Slash Command: ${value}`)
-            const file = require(value);
-            if (!file?.name) return;
-            client.slashCommands.set(file.name, file);
-        
-            if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
-            arrayOfSlashCommands.push(file);
-        })
-    
+    const slashCommands = await globPromise(
+        `${process.cwd()}/SlashCommands/*/*.js`
+    );
 
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.slashCommands.set(file.name, file);
+
+        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+        arrayOfSlashCommands.push(file);
+    });
     client.on("ready", async () => {
+        const guild1 = client.guilds.cache.get("969944638498680872");
+        guild1.commands.set([]);
+
+        const guild2 = client.guilds.cache.get("427865794467069962");
+        guild2.commands.set([]);
+
+        // Register for all the guilds the bot is in
         await client.application.commands.set(arrayOfSlashCommands);
-        console.log("Slash commands registered!")
     });
 };
