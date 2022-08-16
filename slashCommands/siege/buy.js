53fb205da5f3dbac0e41e51076dfc472059cfbda
Kieran <kieranrobson1999@gmail.com>
2022-08-17 00:19:59 +0100

    Add buy slash command

---
+++ slashCommands/siege/buy.js
@@ -0,0 +1,23 @@
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "buy",
+    description: "Buy siege from cd keys",
+    type: 'CHAT_INPUT',
+    /**
+     *
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async (client, interaction, args) => {
+        const embed = new MessageEmbed()
+        .setColor('ORANGE')
+        .setFooter({ text: `Called By: ${interaction.user.tag}`})        
+        .setTimestamp()
+        .setTitle("Purchase")
+        .addField(`While Kieran and others are not affiliated with CD Keys, we have had good expiriences with CD keys, buy siege here: `, `https://www.cdkeys.com/tom-clancy-s-rainbow-six-siege-pc-cd-key-uplay`)
+        interaction.reply({ embeds: [embed] })
+
+    }
+};  
