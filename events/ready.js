c7fc15e12f686501ea78fc861480959c67424190
Kieran <kieranrobson1999@gmail.com>
2022-08-25 13:55:52 +0100

    Update membercounter

--- events/ready.js
+++ events/ready.js
@@ -4,12 +4,11 @@ client.on('ready', () => {
     console.log('Clarence is online')
     client.user.setActivity(`-help`, {type:"WATCHING"})
 
-    setInterval(() =>{
-
-        const memberCount = client.guilds.cache.get('744586833135927366').memberCount;
-        const channel = guild.channels.cache.get('857995452052799538');
-        channel.setName(`Humans: ${memberCount.toLocaleString()}`);
-    }, 600000);
+        setInterval(() =>{
+            const memberCount = client.guilds.cache.get('744586833135927366').memberCount
+            const channel = client.channels.cache.get('857995452052799538');
+            channel.setName(`Discord Members: ${memberCount.toLocaleString()}`);
+        }, 600000);
 
 })
     
