6727d0d6b8903b627ea25190ffb4ef186d45840c
Kieran <kieranrobson1999@gmail.com>
2022-08-27 20:31:10 +0100

    Readd jumbo command

---
+++ commands/general/jumbo.js
@@ -0,0 +1,32 @@
+const { Message, Client, Util } = require("discord.js");
+const { parse } = require("twemoji-parser");
+
+module.exports = {
+    name: "jumbo",
+    aliases: [''],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        const emoji = args[0];
+        if (!emoji) return message.channel.send("No emoji provided!");
+    
+        let custom = Util.parseEmoji(emoji);
+
+        if (custom.id) 
+        {
+            let emoji = (`https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? "gif" : "png"}`);
+            return message.channel.send(emoji);
+        }
+        else 
+        {
+            let parsed = parse(emoji, { assetType: "png" });
+            if (!parsed[0]) return message.channel.send("Invalid emoji!");
+            return message.channel.send(emoji);
+        }
+
+    }
+}
