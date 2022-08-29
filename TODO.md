2e9cd339617a6ebc8dbc95293dec42ea7e90d400
Kieran <kieranrobson1999@gmail.com>
2022-08-29 13:25:49 +0100

    Changed api for six siege stats

---
+++ TODO.md
@@ -0,0 +1,11 @@
+### Pokemon Commands
+
+### Minecraft Commands
+- [ ] Data for blocks
+- [ ] Data for GUIs
+- [ ] Information about current version
+- [ ] Information about porion effects
+
+
+### Siege Commands
+- [ ] Get general stats
--- slashCommands/siege/getstats.js
+++ slashCommands/siege/getstats.js
@@ -1,6 +1,13 @@
 const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const R6 = require('r6s-stats-api');
+require("dotenv").config();
+
+const R6API = require('r6api.js').default;
+email = process.env.UBI_EMAIL
+password = process.env.UBI_PASSWORD
+const r6api = new R6API({ email, password });
+
+
 
 module.exports = {
     ...new SlashCommandBuilder()
@@ -12,18 +19,12 @@ module.exports = {
             .setRequired(true)
         )
         .addStringOption((option) => option
-        .setName('platform')
-        .setDescription("target platform")
-        .setRequired(true)
-        )
-        .addStringOption((option) => option
-            .setName('type')
-            .setDescription('target type')
+            .setName('platform')
+            .setDescription('target platform')
             .addChoices(
-                { name: 'Casual', value: 'casual' },
-                { name: 'Unranked', value: 'unranked' },
-                { name: 'Ranked', value: 'ranked' },
-                { name: 'Deathmatch', value: 'deathmatch' },
+                { name: 'Uplay', value: 'uplay' },
+                { name: 'Xbox', value: 'xbl' },
+                { name: 'PSN', value: 'psn' },
             )),
         
     /**
@@ -36,63 +37,17 @@ module.exports = {
     run: async (client, interaction, args) => {
         const inputUser = interaction.options.getString("user")
         const inputPlatform = interaction.options.getString("platform")
-        const inputType = interaction.options.getString("type")
 
-        if(inputType == null)
-        {
-            const stats = await R6.general(inputPlatform, inputUser);
-            console.log(stats)
-            const embed = new MessageEmbed()
-            .setTitle(`General Stats`)
-            .setColor('ORANGE')
-            .setTimestamp()
-            .setThumbnail(stats.header)
-            .setFooter({ text: `Called By: ${interaction.user.tag}`})
-            .addField(`Username`, `${stats.name}`)
-            .addField(`URL`, `${stats.url}`)
-            .addField(`Level`, `${stats.level}`, true)
-            .addField(`Total XP`, `${stats.total_xp}`, true)
-            .addField(`Matches Played`, `${stats.matches_played}`)
-            .addField(`Time Played`, `${stats.time_played}`)
-            .addField(`KD`, `${stats.kd}`, true)
-            .addField(`Kills`, `${stats.kills}`, true)
-            .addField(`Deaths`, `${stats.deaths}`, true)
-            .addField(`Melee Kills`, `${stats.melee_kills}`)
-            .addField(`Blind Kills`, `${stats.blind_kills}`)
-            .addField(`Win Percentage`, `${stats.win_}`,true)
-            .addField(`Wins`, `${stats.wins}`, true)
-            .addField(`Loses`, `${stats.losses}`, true)
-            .addField(`Headshot Percentage`, `${stats.headshot_}`, true)
-            .addField(`Total Headshots`, `${stats.headshots}`, true)
-            interaction.reply({embeds: [embed]})
-        }
-        else if(inputType == 'casual')
-        {
-            let stats = await R6.casual(inputPlatform, inputUser);
-            const embed = new MessageEmbed()
-            .setTitle(`Casual Stats`)
-            .setColor('ORANGE')
-            .setTimestamp()
-            .setThumbnail(stats.header)
-            .setFooter({ text: `Called By: ${interaction.user.tag}`})
-            .setImage(stats.rank_img)
-            .addField(`Username`, `${stats.name}`)
-            .addField(`URL`, `${stats.url}`)
-            .addField(`MMR`, `${stats.mmr}`, true)
-            .addField(`Rank`, `${stats.rank}`)
-            .addField(`Matches Played`, `${stats.matches}`)
-            .addField(`Time Played`, `${stats.time_played}`)
-            .addField(`KD`, `${stats.kd}`, true)
-            .addField(`Kills`, `${stats.kills}`, true)
-            .addField(`Deaths`, `${stats.deaths}`, true)
-            .addField(`Kills per Match`, `${stats.kills_match}`)
-            .addField(`Kills per Minute`, `${stats.min}`)
-            .addField(`Win Percentage`, `${stats.win_}`,true)
-            .addField(`Wins`, `${stats.wins}`, true)
-            .addField(`Loses`, `${stats.losses}`, true)
+        const { 0: player } = await r6api.findByUsername(inputPlatform, inputUser);
+        if (!player) return 'Player not found';
 
-            await  interaction.reply({embeds: [embed]})
+        const { 0: stats } = await r6api.getStats(inputPlatform, player.id);
+        if (!stats) return 'Stats not found';
+        const { pvp: { general } } = stats;
+        
+        console.log(`${player.username} has played ${general.matches} matches.`) 
+        console.log(`${player.username} has played for ${general.playtime}.`)
+        interaction.reply(`${player.username} has played ${general.matches} matches. and has played for ${general.playtime}`)
 
-        }
     },
 };
