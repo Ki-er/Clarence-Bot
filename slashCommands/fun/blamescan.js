f3c0c05c63c27bfb8dd9f9a5f131d99ac27cb61a
Kieran <kieranrobson1999@gmail.com>
2022-08-31 14:40:04 +0100

    Condensed blamescan

--- slashCommands/fun/blamescan.js
+++ slashCommands/fun/blamescan.js
@@ -1,4 +1,5 @@
 const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
+const { count } = require("node:console");
 const blames = require('../../schemas/blamestitch-schema');
 const wait = require('node:timers/promises').setTimeout;
 
@@ -18,16 +19,33 @@ module.exports = {
         await wait(3000)
         await interaction.editReply(`Stitch has been blamed ${19 + blame.length} times.\r\nThere were 19 blames before the database.\r\nGetting all database blames, this may take a while.`)
 
+        let counter = 0
+        for(let i = 0; i < blame.length; ++i) 
+        {
+            if(counter == blame.length)
+            {
+                break;
+            }
 
-        for(let i = 0; i < blame.length; ++i) {
             const embed = new MessageEmbed()
-                .setColor('ORANGE')
-                .setFooter({ text: `Called By: ${interaction.user.tag}`})                   
-                .setTimestamp()
-                .addField('Reason:', `${blame[i].reason}`)
-                .addField('Date', `${blame[i].date.toLocaleDateString("en-UK")}`)
-                .addField('Added by', `<@${blame[i].userId}>`);
-            interaction.channel.send({ embeds: [embed] });
+            for(let j = 0; j < 10; ++j) 
+            {
+                if(counter == blame.length)
+                {
+                    break;
+                }
+
+                embed.setColor('ORANGE')
+                embed.setFooter({ text: `Called By: ${interaction.user.tag}`})                   
+                embed.setTimestamp()
+                embed.addField(`Blame Number: ${[counter+1]}`, 
+                `Reason: ${blame[counter].reason}
+                Date: ${blame[counter].date.toLocaleDateString("en-UK")}
+                Added by: <@${blame[counter].userId}>`)
+                ++counter
+            }
+                interaction.channel.send({ embeds: [embed] });
+            }
         }
-    }
-};  
+    
+};
--- slashCommands/siege/getstats.js
+++ slashCommands/siege/getstats.js
@@ -40,15 +40,56 @@ module.exports = {
         const inputPlatform = interaction.options.getString("platform")
 
         const { 0: player } = await r6api.findByUsername(inputPlatform, inputUser);
-        if (!player) return 'Player not found';
+        if (!player) return interaction.reply({ contents: "Player not found", ephemeral: true });
 
         const { 0: stats } = await r6api.getStats(inputPlatform, player.id);
-        if (!stats) return 'Stats not found';
+        if (!stats) return interaction.reply({ contents: "Stats not found", ephemeral: true });
         const { pvp: { general } } = stats;
+        console.log(stats)
+
+
+        const embed = new MessageEmbed()
+        .setTitle(`${player.username} (${player.userId})`)
+        .setThumbnail(`https://ubisoft-avatars.akamaized.net/${player.id}/default_146_146.png`)
+        .setTimestamp()
+        .setColor(`ORANGE`)
+        .setFooter({ text: `Called By: ${interaction.user.tag}`})        
+        .addField(`Kill Death Assists`, `- KD: ${general.kd}
+        - Kills: ${general.kills}\r\n- Deaths: ${general.deaths}
+        - Assists: ${general.assists}
+        - Suicides: ${general.suicides}
+        `)
+    
+        .addField(`Win Loss`, 
+        `- Matches: ${general.matches}
+        - Win/Loss: ${general.winRate}
+        - Wins: ${general.wins}
+        - Losses: ${general.losses}`)
+
+        .addField(`Bullet Stats`, 
+        `- Bullets Fired: ${general.bulletsFired}
+        - Bullets Connected: ${general.bulletsConnected}
+        `)
+
+        .addField(`Down Stats`, 
+        `- Down But Not Outs: ${general.dbno}
+        - Down But Not Outs Assists: ${general.dbnoAssists}
+        `)
+
+        .addField(`Misc Stats`, 
+        `- Blind Kills: ${general.blindKills}
+        - Penetration Kills: ${general.penetrationKills}
+        - Melee Kills: ${general.meleeKills}
+        - Rappel Breaches: ${general.rappelBreaches}
+        - Gadgets Destroyed: ${general.gadgetsDestroyed}
+        - Barricades Deployed: ${general.barricadesDeployed}
+        - Reinforcements Deployed: ${general.reinforcementsDeployed}
         
+        `)
+
+        interaction.reply({ embeds: [embed] })
         console.log(`${player.username} has played ${general.matches} matches.`) 
         console.log(`${player.username} has played for ${general.playtime}.`)
-        interaction.reply(`${player.username} has played ${general.matches} matches. and has played for ${general.playtime}`)
 
     },
 };
