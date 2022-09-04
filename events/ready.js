2525c981f855d1666a9fe123076e6770fc711020
Kieran <kieranrobson1999@gmail.com>
2022-09-04 19:21:08 +0100

    Fix membercounter

--- events/ready.js
+++ events/ready.js
@@ -4,11 +4,9 @@ client.on('ready', () => {
     console.log('Clarence is online')
     client.user.setActivity(`Slash Commands`, {type:"WATCHING"})
 
-        setInterval(() =>{
-            const memberCount = client.guilds.cache.get('744586833135927366').memberCount
-            const channel = client.channels.cache.get('857995452052799538');
-            channel.setName(`Discord Members: ${memberCount.toLocaleString()}`);
-        }, 600000);
-
+    setInterval(() =>{
+        const memberCount = client.guilds.cache.get('744586833135927366').memberCount
+        const channel = client.channels.cache.get('857995452052799538');
+        channel.setName(`Discord Members: ${memberCount.toLocaleString()}`);
+    }, 600000);
 })
-    
