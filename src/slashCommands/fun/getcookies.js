a21c8a5eb4e30677479c082bbff3bbf6f44507ec
Kieran <kieranrobson1999@gmail.com>
2022-09-21 14:29:57 +0100

    chore: setup showcookies slash command

---
+++ src/slashCommands/fun/getcookies.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const cookie = require('../../schemas/cookie-schema');
+
+module.exports = {
+	...new SlashCommandBuilder()
+		.setName('showcookies')
+		.setDescription('Output ammount of cookies')
+		.addUserOption((option) =>
+			option
+				.setName('user')
+				.setDescription('user for the details you want')
+				.setRequired(false)
+		)
+		.addStringOption((option) =>
+			option.setName('type').setDescription('special case').setRequired(false)
+		),
+
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {CommandInteraction} interaction
+	 * @param {String[]} args
+	 */
+	run: async (client, interaction) => {
+		const inputUser = interaction.options.getMember('user');
+		const type = interaction.option.getString('type');
+	},
+};
