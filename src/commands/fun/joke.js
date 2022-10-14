ca93d362ede5483c033967ee7a5dcac59a2e3007
Mudit Mahajan <muditmahajan21@gmail.com>
2022-10-14 11:28:35 +0530

    added comments

--- src/commands/fun/joke.js
+++ src/commands/fun/joke.js
@@ -5,6 +5,7 @@ module.exports = {
 	name: 'joke',
 	aliases: ['n'],
 	run: async (client, message) => {
+		// Call the Joke API and get a random joke excluding the NSFW ones and other categories
 		let url = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit`;
 		await axios({
 			method: 'get',
@@ -12,11 +13,14 @@ module.exports = {
 		})
 			.then((response) => {
 				let description = '';
+				// If the joke is a single joke
 				if (response.data.type === 'single') {
 					description = response.data.joke;
 				} else {
+					// If the joke is a two-part joke
 					description = `${response.data.setup}\n${response.data.delivery}`;
 				}
+				// Create an embed with the joke
 				const embed = new MessageEmbed()
 					.setColor('ORANGE')
 					.setFooter({ text: `Called By: ${message.author.tag}` })
@@ -26,6 +30,7 @@ module.exports = {
 				message.channel.send({ embeds: [embed] });
 			})
 			.catch(() => {
+				// If the API is down or something else happens
 				const embed = new MessageEmbed()
 					.setColor('ORANGE')
 					.setFooter({ text: `Called By: ${message.author.tag}` })
--- src/slashCommands/fun/joke.js
+++ src/slashCommands/fun/joke.js
@@ -12,6 +12,7 @@ module.exports = {
 	 * @param {String[]} args
 	 */
 	run: async (client, interaction) => {
+		// Call the Joke API and get a random joke excluding the NSFW ones and other categories
 		let url = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit`;
 		await axios({
 			method: 'get',
@@ -19,11 +20,14 @@ module.exports = {
 		})
 			.then((response) => {
 				let description = '';
+				// If the joke is a single joke
 				if (response.data.type === 'single') {
 					description = response.data.joke;
 				} else {
+					// If the joke is a two-part joke
 					description = `${response.data.setup}\n${response.data.delivery}`;
 				}
+				// Create an embed with the joke
 				const embed = new MessageEmbed()
 					.setColor('ORANGE')
 					.setFooter({ text: `Called By: ${interaction.user.tag}` })
@@ -33,6 +37,7 @@ module.exports = {
 				interaction.reply({ embeds: [embed] });
 			})
 			.catch(() => {
+				// If the API is down or something else happens
 				const embed = new MessageEmbed()
 					.setColor('ORANGE')
 					.setFooter({ text: `Called By: ${interaction.user.tag}` })
