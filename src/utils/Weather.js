cb378e7ec2daba69f810c00db61502d17b864354
Richard Kovaľ <richard.koval@student.tuke.sk>
2022-11-07 16:14:56 +0100

    weather command

---
+++ src/utils/Weather.js
@@ -0,0 +1,5 @@
+//Change first character of string from Lowercase to Uppercase
+exports.ChangeFirstLetterToUpperCase = (string) => {
+    const firstCharOfString = string[0];
+    return string.replace(firstCharOfString,firstCharOfString.toUpperCase());
+}
