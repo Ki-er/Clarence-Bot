e90c00ecb7f66b10434e3ccbb3e723f4481a5863
viktorluk311 <viktor.luk311@gmail.com>
2022-11-07 22:30:04 +0100

    Update userinfo.js

--- src/slashCommands/general/userinfo.js
+++ src/slashCommands/general/userinfo.js
@@ -45,16 +45,28 @@ module.exports = {
 
 			const embed = new MessageEmbed()
 				.setTitle(`${user.tag}`)
-				.setURL(`${user.avatarURL({ dynamic: true })}`)
+				//.setURL(`${user.avatarURL({ dynamic: true })}`)
 				.setColor('ORANGE')
 				.setFooter({ text: `Called By: ${interaction.user.tag}` })
-				.setThumbnail(user.avatarURL({ dynamic: true }))
+				//.setThumbnail(user.avatarURL({ dynamic: true }))
+				.setURL(`${user.displayAvatarURL()}`)
+				.setThumbnail(user.displayAvatarURL())
 				.setTimestamp()
 				.setDescription(
-					`- Known as: ${user}
-				- Joined: ${joinedTime ? time(joinedTime, 'R') : 'Unknown'}
-				- Created: ${user.createdAt ? time(user.createdAt, 'R') : 'Unknown'}`
+				`- Known as: ${user}
+				- Is user bot: ${user.bot ? 'Yes' : 'No'}`)
+				// - Joined: ${joinedTime ? time(joinedTime, 'R') : 'Unknown'}
+				// - Created: ${user.createdAt ? time(user.createdAt, 'R') : 'Unknown'}
+				
+				.addFields(
+					//{ name: 'Known as', value: `${user}`},
+					//{ name: 'Is user bot', value: user.bot ? 'Yes' : 'No' },
+					{ name: 'Joined', value: joinedTime ? time(joinedTime, 'R') : 'Unknown', inline: true },
+					{ name: 'Created', value: user.createdAt ? time(user.createdAt, 'R') : 'Unknown', inline: true }
 				)
+				.setImage(user.displayAvatarURL())
+				.addField('** **', '** **')
+				//.addField('\u200b', '\u200b')
 				.addFields(
 					{
 						name: 'Sent cookies',
@@ -67,7 +79,16 @@ module.exports = {
 						inline: true,
 					}
 				);
+			// if(user.avatarURL == null){
+			// 	embed.setURL(`${user.displayAvatarURL()}`)
+			// 	embed.setThumbnail(user.displayAvatarURL())
+			// }
+			// else{
+			// 	//embed.setURL(`${user.avatarURL()}`)
+			// 	//embed.setThumbnail(user.avatarURL())
+			// }
 			interaction.reply({ embeds: [embed] });
 		});
 	},
 };
+
