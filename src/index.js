20311e2e814de506f69a30d8882aade2d7008ff7
Branislav <63232751+BranicekL@users.noreply.github.com>
2022-11-08 12:15:11 +0100

    Added XP system
    XP system is also toggleable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { Client, Collection } = require('discord.js');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Levels = require('discord-xp');
+
+
 
 const client = new Client({
 	intents: 98819,
@@ -17,6 +20,11 @@ require('./handler')(client);
 
 // Connect to database
 const mongooseConnectionString = process.env.MONGOOSE;
+
+Levels.setURL(mongooseConnectionString);
+
+let isXpSystemOn = true;
+
 mongoose
 	.connect(mongooseConnectionString, {
 		useNewUrlParser: true,
@@ -30,3 +38,23 @@ mongoose
 	});
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", async(message) => {
+    if(message.author.bot) return;
+    if(!message.guild) return;
+
+	if (isXpSystemOn == true){
+		const randomXP = Math.floor(Math.random() * 10) + 1;
+		const hasLevelUp = await Levels.appendXp(message.author.id, message.guild.id, randomXP);
+		console.log(`${message.author.id} recieved ${randomXP} xp!\n`);
+		if(hasLevelUp){
+			const user = await Levels.fetch(message.author.id, message.guild.id);
+			message.channel.send(`You have reached new level! Congratulations! ${user.level}`); 
+		}
+	}
+
+});
+
+client.on("onToggleXp", isOn => {
+	isXpSystemOn = isOn;
+});
