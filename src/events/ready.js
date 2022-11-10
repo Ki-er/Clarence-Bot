120e2981d9e487bb0149443d951902aeed47b8dc
KieranRobson <kieranrobson1999@gmail.com>
2022-11-10 09:36:17 +0000

    chorE: Run format

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,9 +4,9 @@ const guild = require('../schemas/guild-schema');
 client.on('ready', () => {
 	console.log('Clarence is online');
 
-	let serverTotal = 0
-	client.guilds.cache.forEach((guild) => {
-		serverTotal = serverTotal + 1
+	let serverTotal = 0;
+	client.guilds.cache.forEach(() => {
+		serverTotal = serverTotal + 1;
 	});
 
 	const Activities = [`${serverTotal} servers`, 'Slash Commands'];
