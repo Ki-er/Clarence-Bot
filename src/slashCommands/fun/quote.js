74f0161d6292972a50fab3031cb331fa45557179
KieranRobson <kieranrobson1999@gmail.com>
2022-11-10 11:24:32 +0000

    chore: Run format

--- src/slashCommands/fun/quote.js
+++ src/slashCommands/fun/quote.js
@@ -1,33 +1,31 @@
-const {SlashCommandBuilder} = require("@discordjs/builders");
-const {getQuote} = require("../../utils/GoodReads");
+const { getQuote } = require('../../utils/GoodReads');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    ...new SlashCommandBuilder()
-        .setName('quote')
-        .setDescription('Get a Quote')
-        .addStringOption((option) =>
-            option
-                .setName('string')
-                .setDescription('Type of quote')
-                .setRequired(true)
-        ),
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-    run: async (client, interaction) => {
-        const quote_type = interaction.options.getString('string');
+	...new SlashCommandBuilder()
+		.setName('quote')
+		.setDescription('Get a Quote')
+		.addStringOption((option) =>
+			option.setName('string').setDescription('Type of quote').setRequired(true)
+		),
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {CommandInteraction} interaction
+	 * @param {String[]} args
+	 */
+	run: async (client, interaction) => {
+		const quote_type = interaction.options.getString('string');
 
-        await interaction.deferReply()
+		await interaction.deferReply();
 
-        const quote = await getQuote(quote_type)
-        if(quote === null){
-            await interaction.editReply('Quote does not exist.')
-        }else {
-            await interaction.editReply(`> ${quote.text}  \n -${quote.author ?? 'unknown'}`)
-        }
-
-    },
+		const quote = await getQuote(quote_type);
+		if (quote === null) {
+			await interaction.editReply('Quote does not exist.');
+		} else {
+			await interaction.editReply(
+				`> ${quote.text}  \n -${quote.author ?? 'unknown'}`
+			);
+		}
+	},
 };
--- src/utils/GoodReads.js
+++ src/utils/GoodReads.js
@@ -1,57 +1,56 @@
-
 const axios = require(`axios`);
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
+async function getPagesLength(url) {
+	const raw = await axios.get(url);
+	const dom = new JSDOM(raw.data);
 
-async function getPagesLength (url){
-    const raw = await axios.get(url)
-    const dom = new JSDOM(raw.data)
+	let result = 1;
+	const number = dom.window.document.getElementsByClassName('next_page').item(0)
+		?.previousElementSibling?.innerHTML;
 
-    let result = 1
-    const number = dom.window.document.getElementsByClassName('next_page').item(0)?.previousElementSibling?.innerHTML
+	if (number !== undefined && number !== '') {
+		result = parseInt(number, 10);
+	}
 
-    if (number !== undefined && number !== '') {
-        result = parseInt(number, 10)
-    }
-
-    return result
+	return result;
 }
 
-async function getQuoteElements (url) {
-    const raw = await axios.get(url)
-    const dom = new JSDOM(raw.data)
+async function getQuoteElements(url) {
+	const raw = await axios.get(url);
+	const dom = new JSDOM(raw.data);
 
-    return dom.window.document.getElementsByClassName('quoteText')
+	return dom.window.document.getElementsByClassName('quoteText');
 }
-
 
 function between(min, max) {
-    return Math.floor(
-        Math.random() * (max - min) + min
-    )
+	return Math.floor(Math.random() * (max - min) + min);
 }
-
 
 exports.getQuote = async (tag) => {
-    const url = `https://www.goodreads.com/quotes/tag/${tag}`
+	const url = `https://www.goodreads.com/quotes/tag/${tag}`;
 
-    const pages = await getPagesLength(url)
-    const index = between(pages, 1)
+	const pages = await getPagesLength(url);
+	const index = between(pages, 1);
 
-    const quotes = await getQuoteElements(`${url}?page=${index}`)
-    const quoteIndex = Math.random(quotes.length - 1)
+	const quotes = await getQuoteElements(`${url}?page=${index}`);
+	const quoteIndex = Math.random(quotes.length - 1);
 
-    const quoteItem = quotes.item(quoteIndex)
+	const quoteItem = quotes.item(quoteIndex);
 
-    if (quoteItem == null) {
-        return  null
-    }
-    let quoteTexts = quoteItem.firstChild.wholeText.trim().slice(1, -1)
-    let quoteAuthor =  quoteItem?.childNodes.item(3)?.textContent?.trim().split(',')[0].trim()
+	if (quoteItem == null) {
+		return null;
+	}
+	const quoteTexts = quoteItem.firstChild.wholeText.trim().slice(1, -1);
+	const quoteAuthor = quoteItem?.childNodes
+		.item(3)
+		?.textContent?.trim()
+		.split(',')[0]
+		.trim();
 
-    return {
-        text: quoteTexts,
-        author: quoteAuthor ?? null
+	return {
+		text: quoteTexts,
+		author: quoteAuthor ?? null,
+	};
 };
-}
