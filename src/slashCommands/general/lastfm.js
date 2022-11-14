54fda350e3631c34e159e71845c5dc6d86db7c6b
KieranRobson <kieranrobson1999@gmail.com>
2022-11-14 00:00:16 +0000

    chore: boilerplate for subcommand

---
+++ src/slashCommands/general/lastfm.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	...new SlashCommandBuilder()
+		.setName('lastfm')
+		.setDescription('Output lastfm information')
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('user')
+				.setDescription('Output information about a user on lastfm')
+				.addStringOption((option) =>
+					option
+						.setName('username')
+						.setDescxription('The user from lastfm')
+						.setRequired(true)
+				)
+		),
+
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {CommandInteraction} interaction
+	 * @param {String[]} args
+	 */
+
+	run: async (client, interaction) => {},
+};
