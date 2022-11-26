b529b9fa6899300c69aa000289bb8f6b56a95582
KieranRobson <kieranrobson1999@gmail.com>
2022-11-26 13:51:30 +0000

    chore: Run Format

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -2,16 +2,14 @@ const client = require('../index');
 const guildData = require('../schemas/guild-schema');
 
 client.on('guildDelete', async (guild) => {
-    guildData.find().then((data) => {
-        if (!data) return;
+	guildData.find().then((data) => {
+		if (!data) return;
 
-        data.forEach(async (record) => {
-
-            if(record.id == guild.id)
-            {
-                await guildData.deleteOne({ _id: guild.id })
-                console.log(`Deleted ${guild.name} from the database`)
-            }
-        });
-    });
+		data.forEach(async (record) => {
+			if (record.id == guild.id) {
+				await guildData.deleteOne({ _id: guild.id });
+				console.log(`Deleted ${guild.name} from the database`);
+			}
+		});
+	});
 });
